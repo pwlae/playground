0be7984f752c49038e1f7846e53f2ba3a16e7dac
Pavel Danelian <pavel.danelian@gmail.com>
2021-06-05 22:23:56 +0100

    add record creation

--- cloudflare/go.mod
+++ cloudflare/go.mod
@@ -2,4 +2,7 @@ module github.com/pwlae/playground/cloudflare
 
 go 1.16
 
-require github.com/cloudflare/cloudflare-go v0.17.0 // indirect
+require (
+	github.com/cloudflare/cloudflare-go v0.17.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+)
--- cloudflare/main.go
+++ cloudflare/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	cloudflare "github.com/cloudflare/cloudflare-go"
+	uuid "github.com/google/uuid"
 )
 
+type Metadata struct {
+	RedirectUrl string `json:"redirect_url"`
+}
+
 func main() {
+	key := uuid.NewString()
+	log.Println(key)
+
+	redirectUrl := fmt.Sprintf("https://twomenus.io/menu/%s/categories", key)
+	value, err := json.Marshal(&Metadata{
+		RedirectUrl: redirectUrl,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(value)
+
 	// Construct a new API object
 	api, err := cloudflare.New(os.Getenv("CF_API_KEY"), os.Getenv("CF_API_EMAIL"), cloudflare.UsingAccount(os.Getenv("CF_API_ACCOUNT_ID")))
 	if err != nil {
@@ -32,13 +50,23 @@ func main() {
 	}
 
 	// read kv
-	key := "demo"
-	resp, err := api.ReadWorkersKV(context.Background(), os.Getenv("CF_API_WORKER_NAMESPACE_ID"), key)
+	key = "demo"
+	respRead, err := api.ReadWorkersKV(context.Background(), os.Getenv("CF_API_WORKER_NAMESPACE_ID"), key)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%s\n", resp)
+	payload := []byte("test")
+	key = "test"
+
+	respCreate, err := api.WriteWorkersKV(context.Background(), os.Getenv("CF_API_WORKER_NAMESPACE_ID"), key, payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", respRead)
+	fmt.Println(respCreate)
+
 	fmt.Println(lsr)
 	fmt.Println(u)
 }
